901a7323b47059784eda529c1c4f9fd23117d112
wargreymon44 <criminalice2@gmail.com>
2026-03-23 15:27:22 +0700

    feat(columns): generate columns resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UserModule, WorkspacesModule],
+  imports: [AuthModule, UserModule, WorkspacesModule, ColumnsModule],
   controllers: [AppController],
   providers: [AppService],
 })
